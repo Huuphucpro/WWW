11f121c6431f907eda7a4f57b328f2250471f488
HuuphucPro <tranhuuphuc599@gmail.com>
2025-11-09 10:19:42 +0700

    tuan8

---
+++ Tuan8/src/main/java/org/example/tuan8/Tuan8Application.java
@@ -0,0 +1,13 @@
+package org.example.tuan8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuan8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tuan8Application.class, args);
+    }
+
+}
---
+++ Tuan8/src/test/java/org/example/tuan8/Tuan8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tuan8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuan8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
